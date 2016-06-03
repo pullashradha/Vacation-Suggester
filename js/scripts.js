ba375347ec83ba7d9fa376c1a3b5346ef57b4eb5
Shradha Pulla <pullashradha@gmail.com>
2016-06-03 12:33:51 -0700

    Revise js code to make the different destinations show up after taking the survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-$(function(){
-  $("form#content").sumbit(function(event) {
-    $(#japan, #peru, #south-africa).hide();
-
+$(function() {
+  $("form#content").submit(function(event) {
+    // $(#japan, #peru, #south-africa, #switzerland, #stay-home).hide();
+    var firstName = $("input#first-name").val();
+    var lastName = $("input#last-name").val();
     var season = $("select#season").val();
     var dates = $("select#dates").val();
     var budget = $("select#budget").val();
@@ -10,9 +11,25 @@ $(function(){
     var food = $("select#food").val();
     var disability = $("select#disability").val();
 
-    if (season ===)
+    if (firstName && lastName && season && dates && budget && environment && activities && food && disability) {
 
-
+      if (season === "summer" && dates === "5days" || dates === "1week" && budget === "500" || budget === "1000" && environment === "rural" && activities === "nature") {
+        $("#peru").show();
+      } else if (season === "winter" && dates === "5days" || dates === "1week" && budget === "1000" || budget === "2000" && environment === "city" || environment === "both" && activities === "nature" || activities === "shopping") {
+        $("#switzerland").show();
+      } else if (season === "spring" || season === "fall" && dates === "2weeks" || dates === "3weeks" && budget === "2000" || budget === "3000" && environment === "city" && activities === "museum" || activities === "shopping") {
+        $("#japan").show();
+      } else if (season === "spring" || season === "fall" && dates === "2weeks" || dates === "3weeks" && budget === "2000" || budget === "3000" && environment === "both" && activities === "museum" || activities === "nature" || activities === "shopping") {
+        $("#south-africa").show();
+      } else {
+        $("#stay-home").show();
+      };
+    } else {
+      alert("Fill out every section!");
+    };
   event.preventDefault();
   });
 });
+
+
+// (season === "spring" || season === "summer" || season === "fall" || season === "winter" && dates === "5days" || dates === "1week" || dates === "2weeks" || dates === "3weeks" && budget === "500" || budget === "1000" || budget === "2000" || budget === "3000" && environment === "city" || environment === "both" || environment === "rural" && activities === "museum" || activities === "nature" || activities === "shopping") {
