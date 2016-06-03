4822f8aea6cd97d3682d893a119eeeadf8ae17e4
Shradha Pulla <pullashradha@gmail.com>
2016-06-03 10:29:13 -0700

    Add values to dropdown menu questions in index.html, add basic jQuery code to define variables

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+  $("form#content").sumbit(function(event) {
+    $(#japan, #peru, #south-africa, #south-korea).hide();
+
+    var season = $("select#season").val();
+    var dates = $("select#dates").val();
+    var budget = $("select#budget").val();
+    var environment = $("select#environment").val();
+    var activities = $("select#activities").val();
+    var food = $("select#food").val();
+    var disability = $("select#disability").val();
+
+    if (season ===)
+
+
+  event.preventDefault();
+  });
+});
