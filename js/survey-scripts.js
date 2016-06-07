bcee2534bdeeb224327ec997e833612b1d2959af
Shradha Pulla <pullashradha@gmail.com>
2016-06-06 19:16:17 -0700

    Add parentheses to each section in scripts-survey.js to hopefully narrow down survey results restrictions

--- js/survey-scripts.js
+++ js/survey-scripts.js
@@ -13,16 +13,16 @@ $(function() {
 
     if (season && dates && budget && environment && activities && food && disability) {
 
-      if (season === "summer" && dates === "5days" || dates === "1week" && budget === "500" || budget === "1000" && environment === "rural" && activities === "nature") {
+      if (season === "summer" && (dates === "5days" || dates === "1week") && (budget === "500" || budget === "1000") && environment === "rural" && activities === "nature") {
         $("#peru").show();
         $("#content").hide();
-      } else if (season === "winter" && dates === "5days" || dates === "1week" && budget === "1000" || budget === "2000" && environment === "city" || environment === "both" && activities === "nature" || activities === "shopping") {
+      } else if (season === "winter" && (dates === "5days" || dates === "1week") && (budget === "1000" || budget === "2000") && (environment === "city" || environment === "both") && (activities === "nature" || activities === "shopping")) {
         $("#switzerland").show();
         $("#content").hide();
-      } else if (season === "spring" || season === "fall" && dates === "2weeks" || dates === "3weeks" && budget === "2000" || budget === "3000" && environment === "city" && activities === "museum" || activities === "shopping") {
+      } else if ((season === "spring" || season === "fall") && (dates === "2weeks" || dates === "3weeks") && (budget === "2000" || budget === "3000") && environment === "city" && (activities === "museum" || activities === "shopping")) {
         $("#japan").show();
         $("#content").hide();
-      } else if (season === "spring" || season === "fall" && dates === "2weeks" || dates === "3weeks" && budget === "2000" || budget === "3000" && environment === "both" && activities === "museum" || activities === "nature" || activities === "shopping") {
+      } else if ((season === "spring" || season === "fall") && (dates === "2weeks" || dates === "3weeks") && (budget === "2000" || budget === "3000") && environment === "both" && (activities === "museum" || activities === "nature" || activities === "shopping")) {
         $("#south-africa").show();
         $("#content").hide();
       } else {
